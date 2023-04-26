c5db0258592f457ab0c7874aebdc3ece5f1ab1ae
Josue N Rivera <josue.n.rivera@outlook.com>
2023-04-26 14:08:05 -0400

    SoS definition

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+
+from company import Company
+from data import DatasetManager
+from network import SoS
+
+"""
+Author: Josue N Rivera
+"""
+
+__all__ = ["Company", "DatasetManager", "SoS"]
--- src/network.py
+++ src/network.py
@@ -1,5 +1,27 @@
+from typing import List
+
+import torch as th
+
+from company import Company
+
+"""
+Author: Josue N Rivera
+"""
 
 class SoS():
 
-    def __init__(self) -> None:
+    def __init__(self, companies:List[Company]) -> None:
+
+        self.companies = companies
+
+    def set_partners(self):
         pass
+
+    def validate(self, dataset):
+        pass
+
+    def get_names(self) -> List[str]:
+        return [company.name for company in self.companies]
+
+    def get_distribution(self) -> th.Tensor:
+        return th.tensor([company.distribution for company in self.companies])
